389e9d94a83686a6e2b0a5d3b6e5cdd5f17ea5ef
TwinStarBoy <312764087@qq.com>
2018-03-18 11:42:36 +0800

    modify sql script

--- crm-test/src/main/resources/initSql/initSql.sql
+++ crm-test/src/main/resources/initSql/initSql.sql
@@ -7,17 +7,22 @@ drop table if exists CUSTOMER;
 /*==============================================================*/
 create table CUSTOMER
 (
-   ID                   int not null auto_increment comment '����',
-   USERNAME             varchar(50) comment '�û���',
-   PASSWORD             varchar(200) comment '����',
-   MOBILE               varchar(15) comment '��ϵ�ֻ���',
-   IDENTIFICATION       varchar(20) comment '���֤��',
-   EMAIL                varchar(50) comment '����',
-   IS_EMAIL_VERIFY      varchar(1) comment '�Ƿ�������֤(0����1����)',
-   IS_FROZEN            varchar(1) comment '�Ƿ񶳽ᣨ0����1���ǣ�',
-   IS_ENABLE            varchar(1) comment '�Ƿ�ʧЧ��0����1���ǣ�',
-   CREATE_TIME          timestamp comment '����ʱ��',
-   UPDATE_TIME          timestamp comment '�޸�ʱ��',
-   primary key (ID)
+   ID                   int not null auto_increment comment '主键',
+   USERNAME             varchar(50) not null comment '用户名',
+   PASSWORD             varchar(200) not null comment '密码',
+   MOBILE               varchar(15) comment '联系手机号',
+   IDENTIFICATION       varchar(20) comment '身份证号',
+   EMAIL                varchar(50) not null comment '邮箱',
+   IS_EMAIL_VERIFY      varchar(1) comment '是否邮箱验证(0：否，1：是)',
+   IS_FROZEN            varchar(1) comment '是否冻结（0：否，1：是）',
+   IS_ENABLE            varchar(1) comment '是否失效（0：否，1：是）',
+   CREATE_TIME          timestamp default CURRENT_TIMESTAMP comment '创建时间',
+   UPDATE_TIME          datetime default NULL comment '修改时间',
+   primary key (ID),
+   key AK_unique_username (USERNAME),
+   key AK_unique_email (EMAIL)
 );
+
+
+
 
