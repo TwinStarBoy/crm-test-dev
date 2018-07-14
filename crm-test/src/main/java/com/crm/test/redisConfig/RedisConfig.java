7dce14cb9cd32dd3d3c6db67bc47fa0a932bdf48
TwinStarBoy <312764087@qq.com>
2018-07-14 16:19:06 +0800

    modify connect redis code

--- crm-test/src/main/java/com/crm/test/redisConfig/RedisConfig.java
+++ crm-test/src/main/java/com/crm/test/redisConfig/RedisConfig.java
@@ -1,11 +1,14 @@
 package com.crm.test.redisConfig;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.*;
+
+import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * @author 程序猿DD
@@ -15,11 +18,25 @@ import org.springframework.data.redis.serializer.*;
  */
 @Configuration
 public class RedisConfig {
+	@Value("${spring.redis.host}")  
+    private String hostName;  
+    @Value("${spring.redis.port}")  
+    private int port;  
+    @Value("${spring.redis.password}")  
+    private String passWord;  
+    @Value("${spring.redis.pool.max-idle}")  
+    private int maxIdl;  
+    @Value("${spring.redis.pool.min-idle}")  
+    private int minIdl;  
+    @Value("${spring.redis.database}")  
+    private int database;  
+    @Value("${spring.redis.timeout}")  
+    private int timeout;  
 
-    @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        return new JedisConnectionFactory();
-    }
+//    @Bean
+//    JedisConnectionFactory jedisConnectionFactory() {
+//        return new JedisConnectionFactory();
+//    }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
@@ -30,5 +47,24 @@ public class RedisConfig {
         return template;
     }
 
-
+    @Bean  
+    public RedisConnectionFactory jedisConnectionFactory(){  
+        JedisPoolConfig poolConfig=new JedisPoolConfig();  
+        poolConfig.setMaxIdle(maxIdl);  
+        poolConfig.setMinIdle(minIdl);  
+        poolConfig.setTestOnBorrow(true);  
+        poolConfig.setTestOnReturn(true);  
+        poolConfig.setTestWhileIdle(true);  
+        poolConfig.setNumTestsPerEvictionRun(10);  
+        poolConfig.setTimeBetweenEvictionRunsMillis(60000);  
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(poolConfig);  
+        jedisConnectionFactory.setHostName(hostName);   
+        if(!passWord.isEmpty()){   
+            jedisConnectionFactory.setPassword(passWord);   
+        }   
+        jedisConnectionFactory.setPort(port);   
+        jedisConnectionFactory.setDatabase(database);  
+        jedisConnectionFactory.setTimeout(timeout);
+        return jedisConnectionFactory;  
+    } 
 }
